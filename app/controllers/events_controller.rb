63ab47521456b18005b44d42cfa22f5d995c4ee1
Imthiyash <skimthiyash7257@gmail.com>
2025-01-22 10:26:11 +0530

    Users login/logout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!
+    
     def home
         
     end
@@ -8,6 +10,12 @@ class EventsController < ApplicationController
     end
 
     def create
-
+        byebug
+        params.permit(:event_name)
+        event = Event.new(event_name: params[:event_name])
+        unless event.save
+            flash[:alert] = "Event with the same name already exists"
+        end
+        redirect_to events_path
     end
 end
