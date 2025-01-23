32de08ab21bd472062a71e7489fd7bfa596099d3
Puneeth254 <cs21b040@smail.iitm.ac.in>
2025-01-23 23:49:10 +0530

    Events_Login_Merch_Combined

--- db/schema.rb
+++ db/schema.rb
@@ -10,58 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_04_102835) do
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "category_id", null: false
-    t.decimal "price"
-    t.string "size"
-    t.string "color"
-    t.integer "stock_quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-  end
-
-  create_table "sales", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.string "customer_name"
-    t.integer "quantity"
-    t.date "sale_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_sales_on_product_id"
-  end
-
-  create_table "suppliers", force: :cascade do |t|
-    t.string "name"
-    t.string "contact_info"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "products", "categories"
-  add_foreign_key "sales", "products"
-end
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[7.2].define(version: 2025_01_23_171209) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
